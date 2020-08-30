c5f6a343d1710da0e58aa505e7c84887a26e96b7
Dmitrii <dmitrii.zolotuhin@gmail.com>
2020-08-30 23:15:09 +0300

    Added base join to room functionality

---
+++ components/inputBase.tsx
@@ -0,0 +1,37 @@
+import React, {CSSProperties, useRef} from 'react'
+import useHover from '@react-hook/hover'
+
+type InputBaseProps = {
+  placeholder: string
+  onChange(event: React.ChangeEvent<HTMLInputElement>): void
+}
+
+export default function InputBase({ placeholder, onChange }: InputBaseProps) {
+  const target = useRef(null)
+  const isHovering = useHover(target, {enterDelay: 50, leaveDelay: 50})
+
+  return(
+    <input
+      onChange={onChange}
+      placeholder={placeholder}
+      className="placeholder-fix"
+      ref={target}
+      style={{
+        ...ButtonStyles,
+        filter: isHovering ?'brightness(85%)' : 'none'
+      }}
+    />
+  )
+}
+
+const ButtonStyles:CSSProperties = {
+  outline: 'none',
+  height: '48px',
+  width: '145px',
+  marginRight: '10px',
+  padding: '0 0 0 15px',
+  border: 'none',
+  fontWeight: 'bold',
+  borderRadius: '3px ',
+  transition: '0.3s ease'
+}
---
+++ components/joinRoomInputPair.tsx
@@ -0,0 +1,28 @@
+import React, {useState, useCallback} from 'react'
+import { useRouter } from 'next/router'
+import InputBase from "./inputBase";
+import ButtonBase from "./buttonBase";
+
+
+export default function JoinRoomInputPair() {
+  const Router = useRouter()
+  const [roomId, setRoomId] = useState<String>('')
+
+  const isDisabled = useCallback(
+    () => !Boolean(roomId.length),
+    [roomId]
+  )
+
+  const navigateToRoom = async () => {
+     await Router.push(`/room?roomId=${roomId}`, `/${roomId}`, {shallow: true})
+  }
+
+  const  handleCodeInputChange = (event: React.ChangeEvent<HTMLInputElement>):void => setRoomId(event.target.value)
+
+  return(
+    <React.Fragment>
+      <InputBase placeholder="Enter room code" onChange={handleCodeInputChange}/>
+      <ButtonBase onClick={navigateToRoom} flat disabled={isDisabled()}> Join room by code </ButtonBase>
+    </React.Fragment>
+  )
+}
